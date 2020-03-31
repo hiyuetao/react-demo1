453c53cb4352537ba17cc370cbd6f57b0d9cb8f9
hiyuetao <hiyuetao@163.com>
2020-04-01 00:05:10 +0800

    redux

--- src/pages/ReactReduxPage.js
+++ src/pages/ReactReduxPage.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
-
-export default class ReactReduxPage extends Component {
+import { Button } from 'antd'
+import { connect } from 'react-redux'
+class ReactReduxPage extends Component {
   render() {
+    const { counter, add, minus } = this.props;
     return (
       <div>
         <h1>ReactReduxPage</h1>
+        <p>counter：{counter}</p>
+        <Button onClick={add}>+1</Button>
+        <Button onClick={minus}>-1</Button>
       </div>
     )
   }
 }
+
+export default connect(
+  //mapStateToProps
+  state =>{
+    return { counter: state}
+  },
+  // mapDispatchToProps
+  {
+    add:()=>{
+      return {type:'add'}
+    },
+    minus:()=>({type:'minus'})
+  } 
+)(ReactReduxPage)
